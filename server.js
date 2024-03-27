17de4ad78f5f5f9f46e147174c834a0c79390868
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-27 14:15:05 +0200

    Inject frontend to public

--- server.js
+++ server.js
@@ -7,22 +7,20 @@ import { loggerService } from './services/logger.service.js'
 import { toyService } from './services/toy.service.js'
 
 const app = express()
-app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'))
-} else {
-  const corsOptions = {
-    origin: [
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'http://127.0.0.1:5173',
-    ],
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
+const corsOptions = {
+  origin: [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+  ],
+  credentials: true,
 }
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cors(corsOptions))
 
 // GET TOYS
 app.get('/api/toy', (req, res) => {
@@ -119,5 +117,5 @@ app.get('/**', (req, res) => {
   // res.sendFile(path.resolve('public/index.html'))
 })
 
-const PORT = process.env.PORT || 3030
-app.listen(PORT, () => loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`))
+const port = process.env.PORT || 3030
+app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))
