b0e98bcd97491f8b4c2d87032ac1cb02703d8bfb
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-04-01 20:11:39 +0300

    Add toy review functionality

--- api/review/review.controller.js
+++ api/review/review.controller.js
@@ -1,72 +1,68 @@
-import {logger} from '../../services/logger.service.js'
-import {socketService} from '../../services/socket.service.js'
-import {userService} from '../user/user.service.js'
-import {authService} from '../auth/auth.service.js'
-import {reviewService} from './review.service.js'
+import { logger } from '../../services/logger.service.js'
+import { socketService } from '../../services/socket.service.js'
+import { userService } from '../user/user.service.js'
+import { authService } from '../auth/auth.service.js'
+import { reviewService } from './review.service.js'
+import { ObjectId } from 'mongodb'
 
 export async function getReviews(req, res) {
-    try {
-        const reviews = await reviewService.query(req.query)
-        res.send(reviews)
-    } catch (err) {
-        logger.error('Cannot get reviews', err)
-        res.status(400).send({ err: 'Failed to get reviews' })
-    }
+  const { filterBy } = req.query
+
+  try {
+    const reviews = await reviewService.query(filterBy)
+    res.send(reviews)
+  } catch (err) {
+    logger.error('Cannot get reviews', err)
+    res.status(400).send({ err: 'Failed to get reviews' })
+  }
 }
 
 export async function deleteReview(req, res) {
-    try {
-        const deletedCount = await reviewService.remove(req.params.id)
-        if (deletedCount === 1) {
-            res.send({ msg: 'Deleted successfully' })
-        } else {
-            res.status(400).send({ err: 'Cannot remove review' })
-        }
-    } catch (err) {
-        logger.error('Failed to delete review', err)
-        res.status(400).send({ err: 'Failed to delete review' })
+  try {
+    const deletedCount = await reviewService.remove(req.params.id)
+    if (deletedCount === 1) {
+      res.send({ msg: 'Deleted successfully' })
+    } else {
+      res.status(400).send({ err: 'Cannot remove review' })
     }
+  } catch (err) {
+    logger.error('Failed to delete review', err)
+    res.status(400).send({ err: 'Failed to delete review' })
+  }
 }
-
 
 export async function addReview(req, res) {
-    
-    var {loggedinUser} = req
- 
-    try {
-        var review = req.body
-        review.byUserId = loggedinUser._id
-        review = await reviewService.add(review)
-        
-        // prepare the updated review for sending out
-        review.aboutUser = await userService.getById(review.aboutUserId)
-        
-        // Give the user credit for adding a review
-        // var user = await userService.getById(review.byUserId)
-        // user.score += 10
-        loggedinUser.score += 10
+  var { loggedinUser } = req
 
-        loggedinUser = await userService.update(loggedinUser)
-        review.byUser = loggedinUser
+  try {
+    const { txt, aboutToyId } = req.body
 
-        // User info is saved also in the login-token, update it
-        const loginToken = authService.getLoginToken(loggedinUser)
-        res.cookie('loginToken', loginToken)
-
-        delete review.aboutUserId
-        delete review.byUserId
-
-        socketService.broadcast({type: 'review-added', data: review, userId: loggedinUser._id})
-        socketService.emitToUser({type: 'review-about-you', data: review, userId: review.aboutUser._id})
-        
-        const fullUser = await userService.getById(loggedinUser._id)
-        socketService.emitTo({type: 'user-updated', data: fullUser, label: fullUser._id})
-
-        res.send(review)
-
-    } catch (err) {
-        logger.error('Failed to add review', err)
-        res.status(400).send({ err: 'Failed to add review' })
+    const reviewToAdd = {
+      txt,
+      toyId: new ObjectId(aboutToyId),
+      userId: new ObjectId(loggedinUser._id),
     }
-}
 
+    const review = await reviewService.add(reviewToAdd)
+    console.log(`review`, review)
+
+    // User info is saved also in the login-token, update it
+    const loginToken = authService.getLoginToken(loggedinUser)
+    res.cookie('loginToken', loginToken)
+
+    // socketService.broadcast({ type: 'review-added', data: review, userId: loggedinUser._id })
+    // socketService.emitToUser({
+    //   type: 'review-about-you',
+    //   data: review,
+    //   userId: review.aboutUser._id,
+    // })
+
+    // const fullUser = await userService.getById(loggedinUser._id)
+    // socketService.emitTo({ type: 'user-updated', data: fullUser, label: fullUser._id })
+
+    res.send(review)
+  } catch (err) {
+    logger.error('Failed to add review', err)
+    res.status(400).send({ err: 'Failed to add review' })
+  }
+}
--- api/review/review.service.js
+++ api/review/review.service.js
@@ -1,102 +1,108 @@
-import {dbService} from '../../services/db.service.js'
-import {logger} from '../../services/logger.service.js'
-import {asyncLocalStorage} from '../../services/als.service.js'
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
+import { asyncLocalStorage } from '../../services/als.service.js'
 import mongodb from 'mongodb'
-const {ObjectId} = mongodb
+const { ObjectId } = mongodb
 
 async function query(filterBy = {}) {
-    try {
-        const criteria = _buildCriteria(filterBy)
-        const collection = await dbService.getCollection('review')
-        // const reviews = await collection.find(criteria).toArray()
-        var reviews = await collection.aggregate([
-            {
-                $match: criteria
-            },
-            {
-                $lookup:
-                {
-                    localField: 'byUserId',
-                    from: 'user',
-                    foreignField: '_id',
-                    as: 'byUser'
-                }
-            },
-            {
-                $unwind: '$byUser'
-            },
-            {
-                $lookup:
-                {
-                    localField: 'aboutUserId',
-                    from: 'user',
-                    foreignField: '_id',
-                    as: 'aboutUser'
-                }
-            },
-            {
-                $unwind: '$aboutUser'
-            }
-        ]).toArray()
-        reviews = reviews.map(review => {
-            review.byUser = { _id: review.byUser._id, fullname: review.byUser.fullname }
-            review.aboutUser = { _id: review.aboutUser._id, fullname: review.aboutUser.fullname }
-            delete review.byUserId
-            delete review.aboutUserId
-            return review
-        })
+  try {
+    const criteria = _buildCriteria(filterBy)
+    const collection = await dbService.getCollection('reviews')
+    // const reviews = await collection.find(criteria).toArray()
+    var reviews = await collection
+      .aggregate([
+        {
+          $match: criteria,
+        },
+        {
+          $lookup: {
+            localField: 'toyId',
+            from: 'toys',
+            foreignField: '_id',
+            as: 'aboutToy',
+          },
+        },
+        {
+          $unwind: '$aboutToy',
+        },
+        {
+          $lookup: {
+            localField: 'userId',
+            from: 'users',
+            foreignField: '_id',
+            as: 'byUser',
+          },
+        },
+        {
+          $unwind: '$byUser',
+        },
+      ])
+      .toArray()
 
-        return reviews
-    } catch (err) {
-        logger.error('cannot find reviews', err)
-        throw err
-    }
+    reviews = reviews.map(review => {
+      review.aboutToy = {
+        _id: review.aboutToy._id,
+        name: review.aboutToy.name,
+        price: review.aboutToy.price,
+      }
 
+      review.byUser = { _id: review.byUser._id, nickname: review.byUser.fullname }
+
+      delete review.toyId
+      delete review.userId
+
+      return review
+    })
+
+    console.log(`reviews`, reviews)
+    return reviews
+  } catch (err) {
+    logger.error('cannot find reviews', err)
+    throw err
+  }
 }
 
 async function remove(reviewId) {
-    try {
-        const store = asyncLocalStorage.getStore()
-        const { loggedinUser } = store
-        const collection = await dbService.getCollection('review')
-        // remove only if user is owner/admin
-        const criteria = { _id: ObjectId(reviewId) }
-        if (!loggedinUser.isAdmin) criteria.byUserId = ObjectId(loggedinUser._id)
-        const {deletedCount} = await collection.deleteOne(criteria)
-        return deletedCount
-    } catch (err) {
-        logger.error(`cannot remove review ${reviewId}`, err)
-        throw err
-    }
+  try {
+    const store = asyncLocalStorage.getStore()
+    const { loggedinUser } = store
+    const collection = await dbService.getCollection('reviews')
+    // remove only if user is owner/admin
+    const criteria = { _id: new ObjectId(reviewId) }
+    // if (!loggedinUser.isAdmin) criteria.byUserId = new ObjectId(loggedinUser._id)
+    const { deletedCount } = await collection.deleteOne(criteria)
+    return deletedCount
+  } catch (err) {
+    logger.error(`cannot remove review ${reviewId}`, err)
+    throw err
+  }
 }
 
-
 async function add(review) {
-    try {
-        const reviewToAdd = {
-            byUserId: ObjectId(review.byUserId),
-            aboutUserId: ObjectId(review.aboutUserId),
-            txt: review.txt
-        }
-        const collection = await dbService.getCollection('review')
-        await collection.insertOne(reviewToAdd)
-        return reviewToAdd
-    } catch (err) {
-        logger.error('cannot insert review', err)
-        throw err
+  try {
+    const reviewToAdd = {
+      toyId: review.toyId,
+      userId: review.userId,
+      txt: review.txt,
     }
+
+    const collection = await dbService.getCollection('reviews')
+    await collection.insertOne(reviewToAdd)
+    return reviewToAdd
+  } catch (err) {
+    logger.error('cannot insert review', err)
+    throw err
+  }
 }
 
 function _buildCriteria(filterBy) {
-    const criteria = {}
-    if (filterBy.byUserId) criteria.byUserId = ObjectId(filterBy.byUserId)
-    return criteria
+  const criteria = {}
+  if (filterBy.toyId) criteria.toyId = new ObjectId(filterBy.toyId)
+  return criteria
 }
 
 export const reviewService = {
-    query,
-    remove,
-    add
+  query,
+  remove,
+  add,
 }
-
-
